a38f4f5fcb45eb16f362ee630158583e7700a534
svrama03 <sachin.rama@gmail.com>
2025-10-30 09:49:52 -0700

    Update .gitignore to exclude Rust build artifacts and target directories

--- core/target/release/deps/wasm_bindgen_macro_support-d7cbcd67416cedef.d
+++
@@ -1,8 +0,0 @@
-C:\Users\avica\Documents\CSE486\CapstoneP3-x86emulator\core\target\release\deps\wasm_bindgen_macro_support-d7cbcd67416cedef.d: C:\Users\avica\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.104\src\lib.rs C:\Users\avica\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.104\src\parser.rs
-
-C:\Users\avica\Documents\CSE486\CapstoneP3-x86emulator\core\target\release\deps\libwasm_bindgen_macro_support-d7cbcd67416cedef.rlib: C:\Users\avica\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.104\src\lib.rs C:\Users\avica\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.104\src\parser.rs
-
-C:\Users\avica\Documents\CSE486\CapstoneP3-x86emulator\core\target\release\deps\libwasm_bindgen_macro_support-d7cbcd67416cedef.rmeta: C:\Users\avica\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.104\src\lib.rs C:\Users\avica\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.104\src\parser.rs
-
-C:\Users\avica\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.104\src\lib.rs:
-C:\Users\avica\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\wasm-bindgen-macro-support-0.2.104\src\parser.rs:
--- core/target/wasm32-unknown-unknown/release/libweb_x86_core.d
+++
@@ -1,1 +0,0 @@
-C:\Users\avica\Documents\CSE486\CapstoneP3-x86emulator\core\target\wasm32-unknown-unknown\release\libweb_x86_core.rlib: C:\Users\avica\Documents\CSE486\CapstoneP3-x86emulator\core\src\lib.rs
