790425795d3245e0f5a348174f061c2085a448e1
daborgo <diegoborgo14@gmail.com>
2026-02-27 14:46:16 -0700

    fix merge conflicts removing login changes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './index.css'
 import App from './App.tsx'
+import Login from './Login.tsx'
+import Auth from './Auth.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/emulator" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
